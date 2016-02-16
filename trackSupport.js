8bf0ac9a316c9cf23d58446c84b230e9751de381
tbhag <tris1111@gmail.com>
2016-02-16 14:05:59 -0500

    Create TrackSupport.js

---
+++ trackSupport.js
@@ -0,0 +1,67 @@
+var trackSupport = (function() {
+	
+	var me = {};
+	
+	me.emit = function() {
+		return this.data;
+	};
+	
+	me.user = function() {
+		return this.data.userID;
+	};	
+	
+	me.clear = function() {
+		localStorage.removeItem('trackSupport');
+		this.data.runningTests = [];
+	};
+	
+	me.init = function(settings) {
+		var	limit = settings.limit;
+		var tests = settings.tests;
+		if (!localStorage.getItem('trackSupport')) {
+			me.data = {
+				browser: getBrowser(),
+				runningTests: [],
+				userID: generateUserID(),
+				time: getTime()
+			};
+		} else {
+			me.data = JSON.parse(localStorage.getItem('trackSupport'));	
+		}
+		tests.forEach(function(item, index, array) {
+			generateTestData(item.name, item.activeTest, item.activeCell, limit);
+		});
+		localStorage.setItem('trackSupport', JSON.stringify(me.data));
+	};
+	
+	return me;
+
+
+	function generateTestData(name, activeTest, activeCell, limit) {
+		var location = window.location;
+		var data = {
+			activeTest: activeTest,
+			activeCell: activeCell,
+			page: location.hostname + location.pathname + location.search,
+			time: getTime()
+		};
+		if (me.data.runningTests.length > limit) {
+			data.shift();
+		}
+		me.data.runningTests.push(data);
+	}
+
+	function getTime() {
+		var d = new Date();
+		return d.getTime();
+	}
+
+	function generateUserID() {
+		return Math.random().toString(36).substr(2, 5);
+	}
+	
+	function getBrowser() {
+		return navigator.userAgent;
+	}
+	
+}());
